d0050642409cd1afcabd578b32f9742d34b92f24
LondheShubham153 <shubhamnath5@gmail.com>
2025-03-22 10:24:56 +0530

    Added output with name and ip

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,8 @@
 output "ec2_public_ip" {
   value = [
-    for instance in aws_instance.my_instance : instance.public_ip
+    for instance in aws_instance.my_instance : {
+    name = instance.tags.Name   
+    public_ip =    instance.public_ip
+    }
   ]
 }
